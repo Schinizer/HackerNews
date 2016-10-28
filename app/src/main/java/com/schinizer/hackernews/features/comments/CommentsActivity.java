9c0c19b64591014fafd7837f38f8a5ad1f980abc
Schinizer <crimsonbw@gmail.com>
2016-10-28 20:40:03 +0800

    Created comments activity

---
+++ app/src/main/java/com/schinizer/hackernews/features/comments/CommentsActivity.java
@@ -0,0 +1,15 @@
+package com.schinizer.hackernews.features.comments;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.schinizer.hackernews.R;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+    }
+}
